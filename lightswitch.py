1b516dc9245da1b1b4cef0e4ddceafaf838de3d8
StefanNijssen <stefan.nijssen68@gmail.com>
2022-02-10 11:51:46 +0100

    eerste opdracht af

--- lightswitch.py
+++ lightswitch.py
@@ -5,9 +5,19 @@ button = tk.Button(text='...', bg="white", fg="black")
 button.pack(pady = 20, padx = 20)
 
 # schijf hier tussen je code
+button.config(text= 'Turn light on')
+window['bg'] = 'black'
+def clickButton(event):
+    if button.config('text')[-1] == 'Turn light on':
+        button.config(text= 'Turn light off')
+        window['bg'] = 'yellow'
+        print('Light is on')
+    else:
+        button.config(text= 'Turn light on')
+        window['bg'] = 'black'
+        print('Light is off')
 
-
-
+button.bind('<Button-1>', clickButton)
 # schijf hier tussen je code
 
 window.mainloop()
